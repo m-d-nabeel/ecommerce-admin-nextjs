05be459f017165d7ba285ae89bc03e8b3b7541ca
m-d-nabeel <asharnabeel137@gmail.com>
2024-08-19 08:34:15 +0530

    minor package conflict fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,13 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
